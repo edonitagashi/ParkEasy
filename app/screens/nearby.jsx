ce93d358665267552facbebe15651ac5d10b5fd8
edonitagashi <edonita.gashi@student.uni-pr.edu>
2025-10-13 10:25:48 +0200

    Add static map placeholder

--- app/screens/nearby.jsx
+++ app/screens/nearby.jsx
@@ -8,7 +8,15 @@ const Nearby = () => {
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
       <SearchHeader title="Nearby Parkings" />
-    </SafeAreaView>
+
+        <View style={styles.mapContainer}>
+        <Image
+          source={require("../../assets/images/map.png")} 
+          style={styles.mapImage}
+          resizeMode="cover"
+        />
+      </View>
+    </SafeAreaView>    
   );
 };
 
@@ -16,4 +24,7 @@ export default Nearby;
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "#FFF", paddingHorizontal: 20, paddingTop: 10 },
+  mapContainer: { height: 200, marginBottom: 15, borderRadius: 10, overflow: "hidden" },
+  mapImage: { width: "100%", height: "100%" },
+
 });
